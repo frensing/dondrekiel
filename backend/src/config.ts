a002e1e014872dc452f6529c03a3a00e5d326027
frensing <dev@frensing.de>
2025-06-01 20:09:37 +0200

    test db

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
-const SQLITE_PATH = process.env.SQLITE_PATH || "./db.sql";
+const SQLITE_PATH = (process.env.NODE_ENV === "test"
+  ? process.env.TEST_SQLITE_PATH
+  : process.env.SQLITE_PATH) || "db.sqlite";
 
 console.log(`Using port ${PORT} and sqlite ${SQLITE_PATH}`);
 

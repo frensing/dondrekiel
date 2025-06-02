2b3a855703dd958dcd088a71765df29a9a05859c
frensing <dev@frensing.de>
2025-06-02 20:44:49 +0200

    mocking test

--- backend/test/controllers/groups.test.ts
+++ backend/test/controllers/groups.test.ts
@@ -1,19 +1,29 @@
 import supertest from "supertest";
-
+import { describe, expect, jest, test } from "@jest/globals";
 import app from "../../src/app";
-import { describe, expect, test } from "@jest/globals";
 
 const api = supertest(app);
 
 describe("groups endpoint", () => {
+  const users = [{
+    id: 1,
+    name: "Gruppe 1",
+  }];
+
+  jest.mock("../../src/models/groups", () => {
+    const originalModule = jest.requireActual("../../src/models/groups") as object;
+    return {
+      __esModule: true,
+      ...originalModule,
+      getAllGroups: jest.fn(() => users),
+    };
+  });
+
   test("should return a list of groups", async () => {
     const result = await api.get("/api/v1/groups")
       .expect(200)
       .expect("Content-Type", /json/);
 
-    expect(result.body).toEqual([{
-      id: 1,
-      name: "Gruppe 1",
-    }]);
+    expect(result.body).toEqual(users);
   });
 });

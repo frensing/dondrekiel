38e0e79ca5ec2284735ffc0deba8029ce1574036
frensing <dev@frensing.de>
2025-06-02 21:23:28 +0200

    mock for rest endpoint

--- backend/test/controllers/stations.test.ts
+++ backend/test/controllers/stations.test.ts
@@ -1,16 +1,25 @@
 import supertest from "supertest";
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, jest, test } from "@jest/globals";
 import app from "../../src/app";
 
 const api = supertest(app);
 
-describe("stations endpoint", () => {
-  const stations = [{
-    id: 1,
-    name: "Station 1",
-    description: "Beschreibung",
-  }];
+const stations = [{
+  id: 1,
+  name: "Station 1",
+  description: "Beschreibung",
+}];
 
+jest.mock("../../src/models/Station", () => {
+  const originalModule = jest.requireActual("../../src/models/Station") as object;
+  return {
+    __esModule: true,
+    ...originalModule,
+    getAllStations: jest.fn(() => stations),
+  };
+});
+
+describe("stations endpoint", () => {
   test("should retur a list of all stations", async () => {
     const result = await api.get("/api/v1/stations")
       .expect(200)

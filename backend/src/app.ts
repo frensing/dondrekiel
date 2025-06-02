08daec59047c54a3ca432f75001d92e6b380d790
frensing <dev@frensing.de>
2025-06-02 22:02:44 +0200

    serve react frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,9 @@ import stationsRouter from "./controllers/stations";
 
 const app = express();
 
+// serve frontend
+app.use(express.static("dist"));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import config from "./config";
 import app from "./app";
+import { initDb } from "./models/db";
+
+initDb();
 
 app.listen(config.PORT, () => {
   console.log(`Server started on port ${config.PORT}`);

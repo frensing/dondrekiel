d26dc8e448f4966f74df4b16b3c4ca2d7659da2d
frensing <dev@frensing.de>
2025-08-06 18:59:54 +0200

    backend sqlite

--- backend/src/models/Group.ts
+++ backend/src/models/Group.ts
@@ -2,11 +2,11 @@ import * as fs from "node:fs";
 import db from "./db";
 
 export interface Group {
-  id: number;
-  name: string;
-  locationDate: number | null;
-  latitude: number | null;
-  longitude: number | null;
+    id: number;
+    name: string;
+    locationDate: number | null;
+    latitude: number | null;
+    longitude: number | null;
 }
 
 export function addGroup(name: string) {
@@ -19,5 +19,5 @@ export function addGroup(name: string) {
 
 export function getAllGroups() {
   const sql = fs.readFileSync("./sql/groups/getAll.sql", "utf8");
-  return db.prepare(sql).all() as Group[];
+  return db.prepare(sql).all() as unknown as Group[];
 }
